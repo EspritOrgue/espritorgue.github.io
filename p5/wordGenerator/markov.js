b9f12c2b16c2e26fd1853282f98216b432da23fd
EspritOrgue <amir.alwash@etu.unige.ch>
2019-02-05 22:06:37 +0100

    new version of word generator

--- p5/wordGenerator/markov.js
+++ p5/wordGenerator/markov.js
@@ -1,70 +1,79 @@
 class Markov{
 	constructor(nam, ord, opt){
-  	this.names = nam;
+		this.names = nam;
 		this.order = ord;
 		this.ngrams = {};
 		this.beginnings = [];
-    
-    if(typeof opt === 'string') opt = [opt]
-      
-    this.opt = opt;
-  }
-  
-  setup(){
-    this.ngrams = {};
-    this.beginnings = [];
-    for (var j = 0; j < this.names.length; j++) {
-    	var txt = this.names[j];
-    	for (var i = 0; i <= txt.length - this.order; i++) {
-      	var gram = txt.substring(i, i + this.order);
-      	if (i == 0) {
-        	this.beginnings.push(gram);
-      	}
 
-      	if (!this.ngrams[gram]) {
-        	this.ngrams[gram] = [];
-      	}
-      	this.ngrams[gram].push(txt.charAt(i + this.order));
-   	 }
-  	}
-  }
-  createbutton(){
-    var list = select("#but-list");
-    var button = createButton(this.opt[0] || "Generate");
-    button.elt.title = this.opt[1] || "Generate a new word";
-    list.child(button);
-  	button.mousePressed(() => {
-  		var currentGram = random(this.beginnings);
-  		var result = currentGram;
-  		while(true) {
-    		var possibilities = this.ngrams[currentGram];
-    		if (!possibilities) {
-      		break;
-    		}
-    		var next = random(possibilities);
-    		result += next;
-    		var len = result.length;
-    
-    		if(next == "")break;
-    
-    		currentGram = result.substring(len - this.order, len);
-  		}
-      if(!check.elt.checked && (this.names.includes(result) || this.names.includes(result.toLowerCase()))){
-        return;
-      }
-      result = this.upper(result);
-      (this.opt[1])?result=this.opt[2]+" "+result:false;
-  		var p = createP(result);
-      var list = select("#name-list");
-      list.elt.insertBefore(p.elt,list.elt.firstChild);
+		if(typeof opt === 'string') opt = [opt]
+
+		this.opt = opt;
+	}
+
+	setup(){
+		this.ngrams = {};
+		this.beginnings = [];
+		for (var j = 0; j < this.names.length; j++) {
+			var txt = this.names[j];
+			for (var i = 0; i <= txt.length - this.order; i++) {
+				var gram = txt.substring(i, i + this.order);
+				if (i == 0) {
+					this.beginnings.push(gram);
+				}
+
+				if (!this.ngrams[gram]) {
+					this.ngrams[gram] = [];
+				}
+				this.ngrams[gram].push(txt.charAt(i + this.order));
+			}
+		}
+	}
+	createbutton(){
+		var list = select("#but-list");
+		var button = createButton(this.opt[0] || "Generate");
+		button.elt.title = this.opt[1] || "Generate a new word";
+		list.child(button);
+		button.mousePressed(() => {
+			var currentGram = random(this.beginnings);
+			if(currentGram != undefined){
+				var result = "";
+				result = currentGram;
+				while(true) {
+					var possibilities = this.ngrams[currentGram];
+					if (!possibilities) {
+						break;
+					}
+					var next = random(possibilities);
+					result += next;
+					var len = result.length;
+
+					if(next == "")break;
+
+					currentGram = result.substring(len - this.order, len);
+				}
+				var list = select("#name-list");
+				if(!check.elt.checked && (this.names.includes(result) || this.names.includes(result.toLowerCase()))){
+					var p = createP("<i>Aucun résultat n'a pu être généré. Il est possible que le niveau de rafinement soit trop élevé.</i>");
+
+					list.elt.insertBefore(p.elt,list.elt.firstChild);
+					return;
+				}
+				result = this.upper(result);
+				(this.opt[1])?result=this.opt[2]+" "+result:false;
+				var p = createP(result);
+				list.elt.insertBefore(p.elt,list.elt.firstChild);
+			}
 		});
-  }
-  change_order(newer){
-  	this.order = newer;
-    this.setup();
-  }
-  upper(string) 
+	}
+	change_order(newer){
+		this.order = newer;
+		this.ngrams = {};
+		this.beginnings = [];
+		var status = select("#status")
+		status.html("| Aucun fichier analysés")
+	}
+	upper(string)
 	{
-    return string.charAt(0).toUpperCase() + string.slice(1);
+		return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 }
--- p5/wordGenerator/sketch.js
+++ p5/wordGenerator/sketch.js
@@ -1,109 +1,106 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/9r8CmofnbAQ
+// Based on the work of Daniel Shiffman (http://codingtra.in)
+// Code from: https://youtu.be/9r8CmofnbAQ
 
-//var txt = "the theremin is theirs, ok? yes, it is. this is a theremin.";
-//var txt = "The unicorn is a legendary creature that has been described since antiquity as a beast with a large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization and was mentioned by the ancient Greeks in accounts of natural history by various writers, including Ctesias, Strabo, Pliny the Younger, and Aelian.[1] The Bible also describes an animal, the re'em, which some translations have erroneously rendered with the word unicorn.[1] In European folklore, the unicorn is often depicted as a white horse-like or goat-like animal with a long horn and cloven hooves (sometimes a goat's beard). In the Middle Ages and Renaissance, it was commonly described as an extremely wild woodland creature, a symbol of purity and grace, which could only be captured by a virgin. In the encyclopedias its horn was said to have the power to render poisoned water potable and to heal sickness. In medieval and Renaissance times, the tusk of the narwhal was sometimes sold as unicorn horn.";
-var names_en,name_fr,dico_fr, dico_en,
-    markov_fr, markov_en, markov_dico, markov_dico_en,markov_loth;
-var kaamelott;
+
+var names = [], files = [], list = [], markov =[], check;
 var order = 3, span_order="3";
-var check;
 var path = "dicos/";
 
 function preload() {
-  names_en = loadStrings(path+'poke-en.txt');
-  names_fr = loadStrings(path+'poke-fr.txt');
-  dico_fr = loadStrings(path+'dico-fr.txt');
-  dico_en = loadStrings(path+'dico-en.txt');
-  kaamelott = loadStrings(path+'loth.txt');
+   files = ['poke-en.txt','poke-fr.txt','dico-en.txt','dico-fr.txt','loth.txt'];
+   for(var i= 0; i < files.length; i++){
+      names[i] = loadStrings(path+files[i]);
+   }
+   list = [
+      ["Generate an english Pokemon name","Generate a new Pokémon name based on the 7 first generation","[poke-en]"],
+      ["Générer un nom de Pokémon français","Générer un nom de Pokémon basé sur les 7 premières générations","[poke-fr]"],
+      ["Generate an english word","Generate an english word from a large databse","[dico-en]"],
+      ["Générer un mot français","Générer un mot à partir du dictionnaire français","[dico-fr]"],
+      ["Générer une citation de Loth d'Orcanie","Générer un citaton dite apr Loth d'Orcanie","[kaamelott-loth]"]
+   ];
 }
 
 function setup() {
-  noCanvas();
+   noCanvas();
+   var container = select("#infos");
+   var p = createSpan(names.length+" fichiers/"+files.length+" ont été parsés ");
+   container.child(p);
 
-  markov_en = new Markov(names_en,
-  	order,
-    ["Generate an english Pokemon name",
-    	"Generate a new Pokémon name based on the 7 first generation",
-      "[poke-en]"]);
+   for(var i=0; i< names.length; i++){
+      markov[i] = new Markov(names[i],order,list[i]);
+      markov[i].setup();
+   }
 
-
-
-  markov_dico_en = new Markov(dico_en,
-  	order,
-    ["Generate an english word",
-     "Generate an english word from a large databse",
-    	"[dico-en]"]);
-
-
-
-  markov_fr = new Markov(names_fr,
-  	order,
-    ["Générer un nom de Pokémon français",
-    	"Générer un nom de pokémon basé sur les 7 premières générations",
-      "[poke-fr]"]);
-
-
-
-  markov_dico = new Markov(dico_fr,
-  	order,
-    ["Générer un mot français",
-     "Générer un mot à partir du dictionnaire français",
-    	"[dico-fr]"]);
-
-
-  markov_loth = new Markov(kaamelott,
-  	order,
-    ["Générer une citation de Loth d'Orcanie",
-     "Générer un citaton dite apr Loth d'Orcanie",
-    	"[kaamelott-loth]"]);
-
-  setup_markov();
-  markov_en.createbutton();
- 	markov_dico_en.createbutton();
-  markov_fr.createbutton();
-  markov_dico.createbutton();
-  var list = select("#but-list");
-  var h2 = createElement("h2","Kaamelott");
-  list.child(h2);
-  markov_loth.createbutton();
-
-
-  var br = createElement("br");
-  check = createInput("","checkbox");
-  h2 = createElement("h2","Raffiner l'algorithme");
-  var sp = createSpan("Inclure les mots de la liste de base");
-  var slid = createSlider(2,20,3,1);
-  slid.input(f);
-  span_order = createSpan(slid.value());
-  list.child(br);
-  list.child(check);
-  list.child(sp);
-  list.child(h2);
-  list.child(slid);
-  list.child(span_order);
-  var header = select("#header");
-  var lis = select("#name-list");
-  var divHeight = windowHeight - header.elt.offsetHeight - 20+"px";
-  lis.style("height",divHeight);
-
+   p = createSpan("| Tous les fichiers parsés ont été analysés");
+   p.elt.id = "status"
+   container.child(p);
+   createInterface();
 
 }
-function setup_markov(){
-	markov_en.setup();
-  markov_dico_en.setup();
-  markov_fr.setup();
-  markov_dico.setup();
-  markov_loth.setup();
+function createInterface(){
+   var but_cont = select("#but-list");
+   but_cont.html("");
+   var h2 = createElement("h2","Liste des dictionnaires possibles");
+   but_cont.child(h2);
+
+   for(var i=0; i< names.length; i++){
+      markov[i].createbutton();
+   }
+
+   var br = createElement("br");
+   check = createInput("","checkbox");
+   var sp = createSpan("Inclure les mots de la liste de base");
+   but_cont.child(br);
+   but_cont.child(check);
+   but_cont.child(sp);
+
+
+   h2 = createElement("h2","Raffiner le niveau de l'algorithme");
+   var slid = createSlider(2,20,order,1);
+   slid.input(f);
+   span_order = createSpan(slid.value());
+   var btn = createInput("Regenerate data", "button");
+   btn.mousePressed(() => {
+      var regen_btn = select("#regen");
+      regen_btn.elt.disabled = true;
+      var bt_list = document.getElementById("but-list");
+      var buttons = bt_list.getElementsByTagName("button");
+      for(var i=0;i<buttons.length;i++){
+         buttons[i].disabled=false;
+      }
+      for(var i=0; i< names.length; i++){
+         markov[i] = new Markov(names[i],order,list[i]);
+         markov[i].setup();
+      }
+      var status = select("#status")
+      status.html("| Tous les fichiers ont été analysés")
+      createInterface();
+   });
+   btn.elt.id="regen";
+   var regen_btn = select("#regen");
+
+   btn.elt.disabled = true;
+   but_cont.child(h2);
+   but_cont.child(slid);
+   but_cont.child(span_order);
+   but_cont.child(btn);
+
+   var header = select("#header");
+   var name_cont = select("#name-list");
+   var divHeight = windowHeight - header.elt.offsetHeight - 20+"px";
+   name_cont.style("height",divHeight);
 }
 function f(){
-  order = this.value();
-  span_order.html(this.value());
-  markov_en.change_order(this.value());
-  markov_dico_en.change_order(this.value());
-  markov_fr.change_order(this.value());
-  markov_dico.change_order(this.value());
-  markov_loth.change_order(this.value());
+   order = this.value();
+   span_order.html(order);
+   for(var i=0; i< names.length; i++){
+      markov[i].change_order(order);
+      var regen_btn = select("#regen");
+      regen_btn.elt.disabled = false;
+   }
+   var bt_list = document.getElementById("but-list");
+   var buttons = bt_list.getElementsByTagName("button");
+   for(var i=0;i<buttons.length;i++){
+      buttons[i].disabled=true;
+   }
 }
